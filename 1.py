ec7c66b298c2da7be01640e07098732322c88727
Piff <nope>
2022-12-09 19:46:31 +0100

    .)

--- 1.py
+++ 1.py
@@ -1,20 +1,29 @@
+#nacteni dat a vytvoreni listu
+with open('1input.in') as file:
+    data = [i for i in file.read().strip().split("\n")]
 
-
-list = []
-for line in open('1input.txt','r'):
-    list.append(line.strip())
-
-result = [[]]
-for i in list:
-    if not i:
-        result.append([])
+#secteni jednotlivych batohu, prevedeni do int a nalezeni nejvetsiho batohu
+max = 0
+max2 = 0
+max3 = 0
+count = 0
+for i in data:
+    if i == '':
+        count = 0
     else:
-        result[-1].append(int(i))
+        num = int(i)
+        count += num
 
-add = []
-for i in range(len(result)):
-	sum += result[i]
+    if count > max:
+        max3 = max2
+        max2 = max
+        max = count
+    elif count > max2:
+        max3 = max2
+        max2 = count
+    elif count > max3:
+        max3 = count
+        
 
-print(add) 
-#print(result.sort())
-#print((res[-1]))
+print("Answer to part 1 is:", max)
+print("Answer to part 2 is:", max+max2+max3)
