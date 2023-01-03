98c2bb20a58983910459581a189856e38f90793e
piff <>
2023-01-03 19:57:40 +0100

    finished 6

--- 6.py
+++ 6.py
@@ -1,6 +1,17 @@
-with open('5input.txt') as file:
-    data = [i for i in file.read()]
+with open('6input.txt') as file:
+    data = file.read()
 
+#pro kazde i v rozsahu 4 az maximum: s je set z data v rozsahu po i a 4 jeho predchudce, pokud je set s dlouhy 4 znaky, vypis odpoved. (sety nepovoluji duplikaty, takze jakmile najdu set s delkou 4, nalezl jsem marker)
+#==========part1=========================
+for i in range(4, len(data)):
+    s = set(data[(i-4):i])
+    if len(s) == 4:
+        print("Answer to part1 is:", i)
+        break
 
-for marker in data:
-    
+#==========part2=========================
+for i in range(14, len(data)):
+    s = set(data[(i-14):i])
+    if len(s) == 14:
+        print("Answer to part2 is:", i)
+        break
